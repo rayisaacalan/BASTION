d0d189bc3321f7b6cd905444ce121218e39540b0
Isaac Ray <null@tamu.edu>
2021-11-21 02:24:27 -0600

    Add function to merge 2 disconnected subgraphs to graphs.h header file

--- src/graph.h
+++ src/graph.h
@@ -1,6 +1,7 @@
 // Reinventing the wheel, 1 header file at a time
 // TODO: Find a way to do all this using the BOOST C++ libraries
 #include <vector>
+#include <algorithm>
 #include <Rcpp.h>
 
 // For each vertex, we want to store every outbound edge and its weight (int and double)
@@ -22,6 +23,14 @@ protected:
   // TODO: assess performance versus adjacency matrix representation
   std::vector<std::vector<Pair>> adjac_list;
 public:
+  // Construct an empty graph
+  Graph() {
+
+  }
+  // Construct a graph based on an adjacency list
+  Graph(std::vector<std::vector<Pair>> inp_adjac_list) {
+    adjac_list = inp_adjac_list;
+  }
   // Constructor based on a vector of edges {(parent, child, weight)}
   // and how many total vertices are in the graph (size of exterior vector)
   Graph(std::vector<Edge> &E, int numVertices) {
@@ -40,3 +49,19 @@ public:
   }
 
 };
+
+// Merging two disconnected subgraphs using an edge
+Graph mergeDisconnectedSubGraphs(Graph &g1, Graph &g2, Edge e) {
+  // Create merged adjacency list
+  std::vector<std::vector<Pair>> new_adjac_list(g1.getGraph());
+  // Create space to store result
+  new_adjac_list.reserve(g1.nVertex() + g2.nVertex());
+  // Append the vertices from g2 onto g1
+  std::vector<std::vector<Pair>> g2_adjac_list = g2.getGraph();
+  new_adjac_list.insert(new_adjac_list.end(), g2_adjac_list.begin(), g2_adjac_list.end());
+  // Create new graph with merged adjacency list
+  Graph new_graph(new_adjac_list);
+  new_graph.addEdge(e.parent, e.child, e.weight);
+  return(new_graph);
+}
+
