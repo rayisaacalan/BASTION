202552c076e054a612c1c91d54e2c62be0a31190
Isaac Ray <null@tamu.edu>
2021-11-21 23:34:14 -0600

    Add constructGraph R function skeleton & starter documentation

---
+++ R/functions.R
@@ -0,0 +1,13 @@
+#' Construct a Graph Object from Coordinate Data using K-Nearest Neighbors
+#'
+#' @param coordinates Data frame whose first 2 columns are numeric coordinate data
+#' @param k Integer, the parameter for the K-Nearest Neighbors algorithm
+#'
+#' @return An object of class 'graph' from the igraph package
+#' @export
+#'
+#' @examples
+constructGraph = function(coordinates, k) {
+
+}
+}
