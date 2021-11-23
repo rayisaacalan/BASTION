fb575bad8d89e45bc90381ca178ead7a584382d1
Isaac Ray <null@tamu.edu>
2021-11-22 21:54:04 -0600

    Add constructClusters R function skeleton & starter documentation

--- R/functions.R
+++ R/functions.R
@@ -10,4 +10,18 @@
 constructGraph = function(coordinates, k) {
 
 }
+
+#' Construct a cluster membership list for a graph
+#'
+#' @param graph An object of class 'graph' from the igraph package
+#' @param nclust An integer, the number of different clusters to assign points to. Must be at most N (the number of vertices in graph)
+#'
+#' @return A vector of integers of length N with nclust unique integers
+#' @export
+#'
+#' @examples
+constructClusters = function(graph, nclust) {
+
+}
+
 }
