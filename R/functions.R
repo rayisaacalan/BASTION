573605d693a1bb119b041aa426cefc63d05241ef
Isaac Ray <null@tamu.edu>
2021-11-23 23:28:30 -0600

    Add functionality to constructClusters R function and update documentation to reflect membership being 1-indexed

--- R/functions.R
+++ R/functions.R
@@ -33,8 +33,9 @@ constructGraph = function(coordinates, k) {
 
 #' Construct a cluster membership list for a graph
 #'
-#' @param graph An object of class 'graph' from the igraph package
+#' @param graph An object of class 'graph' from the igraph package. The graph must have weights for each edge
 #' @param nclust An integer, the number of different clusters to assign points to. Must be at most N (the number of vertices in graph)
+#' @param minclust An integer, the smallest allowable cluster size. By default, one-tenth the ratio of vertices to nclust.
 #'
 #' @return A list containing two elements:
 #' 'graph': The input graph with inter-cluster edges inactive
@@ -42,15 +43,53 @@ constructGraph = function(coordinates, k) {
 #' @export
 #'
 #' @examples
-constructClusters = function(graph, nclust) {
-
+constructClusters = function(graph, nclust, minclust = NULL) {
+  # First, get the number of vertices
+  N = vcount(graph)
+  # Check that nclust is valid
+  if(nclust > N) {
+    stop("nclust cannot be larger than the number of vertices in graph")
+  }
+  if(nclust < 1) {
+    stop("nclust must be positive and at least 1")
+  }
+  # Check that minclust is either not provided (in which case initialize)
+  if(is.null(minclust)) {
+    # Each cluster must contain a minimum of ~10% of the points by default
+    minclust = floor((N/nclust) * 0.1)
+  } else if(minclust < 1) {
+    # Can't have 0 size clusters
+    stop("minimum cluster size cannot be less than 1")
+  } else if((minclust * nclust) > N) {
+    # Can't have every cluster so large that there are insufficient vertices
+    stop("minimum cluster size is too large; decrease it or nclust or add more vertices")
+  }
+  # Allocate the membership vector
+  membership = rep(0, N)
+  # Construct minimum spanning tree on the graph
+  minspantree = mst(graph)
+  # Do the following at least once and until all the clusters meet the minimum size requirement:
+  repeat {
+    # Choose nclust - 1 edges to delete from minspantree
+    # Note: by definition a minimum spanning tree will have (N - 1) edges
+    deleted_edge_ids = sample(1:(N - 1), nclust - 1, replace = FALSE)
+    # Create a new graph with nclust spanning trees (a forest)
+    spanforest = delete.edges(minspantree, deleted_edge_ids)
+    # Update the membership of each vertex
+    membership = components(spanforest)$membership
+    # If all the clusters meet the size requirement, we are done
+    if(min(table(membership)) >= minclust) {
+      break
+    }
+  }
+  return(membership)
 }
 
 #' Perform a cluster birth operation (split an existing cluster)
 #'
 #' @param graph An object of class 'graph' from the igraph package
 #' @param membership A vector of integers of length N with k unique integers (k < N) which map each vertex to a cluster
-#' @param clust Integer, the cluster to split. Must be between 0 and k - 1
+#' @param clust Integer, the cluster to split. Must be between 1 and k
 #'
 #' @return A list containing two elements:
 #' 'graph': The input graph with 1 fewer active edge
@@ -66,7 +105,7 @@ graphBirth = function(graph, membership, clust) {
 #'
 #' @param graph An object of class 'graph' from the igraph package
 #' @param membership A vector of integers of length N with k unique integers (1 < k <= N) which map each vertex to a cluster
-#' @param clust Integer, the cluster to split. Must be between 0 and k - 1
+#' @param clust Integer, the cluster to split. Must be between 1 and k
 #'
 #' @return A list containing two elements:
 #' 'graph': The input graph with 1 additional active edge
