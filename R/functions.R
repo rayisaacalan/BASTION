ca37d52aa2afbf05502ab6c79bd885c341a1c6a7
Isaac Ray <null@stat.tamu.edu>
2021-12-07 22:43:33 -0600

    Expand documentation for constructGraph

--- R/functions.R
+++ R/functions.R
@@ -1,10 +1,28 @@
 #' Construct a Graph Object from Coordinate Data using K-Nearest Neighbors
 #'
+#' \code{constructGraph} takes numeric data (currently it requires 2 dimensional data such as spatial data,
+#' this functionality will expand in a future update) and uses the K-Nearest Neighbors algorithm to
+#' construct a graph object from \code{\link[igraph]{igraph}}. The K-Nearest Neighbors implementation is from
+#' \code{\link[cccd]{cccd}}.
+#'
 #' @param coordinates Data frame whose first 2 columns are numeric coordinate data
 #' @param k Integer, the parameter for the K-Nearest Neighbors algorithm
 #'
-#' @return An object of class 'graph' from the igraph package
+#' @return An object of class 'graph' from the \code{\link[igraph]{igraph}} package
 #' @export
+#'
+#' @references
+#' Csardi G, Nepusz T: The igraph software package for complex network research,
+#' InterJournal, Complex Systems 1695. 2006. https://igraph.org
+#'
+#' David J. Marchette (2015). cccd: Class Cover Catch Digraphs. R package version 1.5.
+#' https://CRAN.R-project.org/package=cccd
+#'
+#' Luo, Z.T. (*), Sang, H. and Mallick, B.K. (2021), BAST: Bayesian Additive Regression Spanning Trees
+#' for Complex Constrained Domain
+#'
+#' Luo, Z.T. (*), Sang, H. and Mallick, B.K. (2021), A Bayesian Contiguous Partitioning Method for
+#' Learning Clustered Latent Variables, Journal of Machine Learning Research, 22, 1-52.
 #'
 #' @examples
 #' set.seed(1)
