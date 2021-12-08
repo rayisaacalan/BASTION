68e5de534955c4248e42fbd7440bd416188beb5a
Isaac Ray <null@stat.tamu.edu>
2021-12-07 23:44:13 -0600

    Expand documentation for graphChange

--- R/functions.R
+++ R/functions.R
@@ -380,14 +380,29 @@ graphDeath = function(graph, membership, full_graph) {
 
 #' Perform a cluster death operation followed by a cluster birth operation
 #'
-#' @param graph An object of class 'graph' from the igraph package
+#' \code{graphChange} executes \code{\link{graphDeath}} on the inputs and then executes \code{\link{graphBirth}}
+#' on the output of \code{\link{graphDeath}}. The net effect is that the output graph has the same number of clusters as
+#' the input graph, but different vertex membership. This is to encourage a better mixing time of the Markov chain when this
+#' function is used to implement an MCMC method.
+#'
+#' @param graph An object of class 'graph' from the \code{\link[igraph]{igraph}} package
 #' @param membership A vector of integers of length N with k unique integers (1 < k <= N) which map each vertex to a cluster
-#' @param full_graph An object of class 'graph' from the igraph package; 'graph' should be a subgraph of full_graph
+#' @param full_graph An object of class 'graph' from the \code{\link[igraph]{igraph}} package; 'graph' should be a subgraph of full_graph
 #'
 #' @return A list containing two elements:
 #' \item{graph}{The input graph with the different set of active edges}
 #' \item{membership}{A vector of integers of length N with k unique integers which map each vertex to a cluster, likely different than input membership}
 #' @export
+#'
+#' @seealso
+#' \code{\link{constructClusters}}, \code{\link{graphDeath}}, \code{\link{graphBirth}}, \code{\link{graphHyper}}
+#'
+#' @references
+#' Luo, Z.T. (*), Sang, H. and Mallick, B.K. (2021), BAST: Bayesian Additive Regression Spanning Trees
+#' for Complex Constrained Domain
+#'
+#' Luo, Z.T. (*), Sang, H. and Mallick, B.K. (2021), A Bayesian Contiguous Partitioning Method for
+#' Learning Clustered Latent Variables, Journal of Machine Learning Research, 22, 1-52.
 #'
 #' @examples
 #' set.seed(1)
